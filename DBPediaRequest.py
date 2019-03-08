10bea02978a21162e21861cedbde204053cadd79
daanwerf <daanvanderwerf@hotmail.com>
2019-03-08 19:37:56 +0100

    Implemented code for finding all relevant concepts for a query, based on dbpedia and dbspotlight

--- DBPediaRequest.py
+++ DBPediaRequest.py
@@ -1,55 +1,30 @@
 import requests
-import sys
 
 
 class DBPediaRequest:
-    def __init__(self, subject):
-        self.redirected = False
-        subject_underscore = subject.replace(' ', '_')
-        self.dbpedia_url = 'http://dbpedia.org/data/' + subject_underscore + '.json'
+    def __init__(self, uri):
+        self.dbpedia_url = 'http://dbpedia.org/data/' + uri + '.json'
 
         data = requests.get(self.dbpedia_url).json()
-        try:
-            self.resource = data['http://dbpedia.org/resource/' + subject.replace(' ', '_')]
-        except KeyError:
-            print('This dbpedia entry does not exist')
-            self.resource = ""
+        self.resource = data['http://dbpedia.org/resource/' + uri]
 
-        # If a common misspelling of a query is encountered, go to the redirected (and thus correct) page
-        if len(self.get_related_subject_list()) is 7:
-            new_resource = self.resource['http://dbpedia.org/ontology/wikiPageRedirects'][0]['value']
-            new_resource_split = new_resource.split('/')
-            new_subject = new_resource_split[len(new_resource_split) - 1]
-
-            self.dbpedia_url = 'http://dbpedia.org/data/' + new_subject + '.json'
-            data = requests.get(self.dbpedia_url).json()
-
-            try:
-                self.resource = data['http://dbpedia.org/resource/' + new_subject]
-                self.redirected = True
-            except KeyError:
-                print('This dbpedia entry does not exist')
-                sys.exit()
-
-    def get_related_subject_list(self):
+    def get_related_subject_list(self) -> list:
         result_list = []
+
         for key in sorted(self.resource):
             key_split = key.split('/')
             result_list.append(key_split[len(key_split)-1])
 
         return result_list
 
-    def get_all_information_on_subject(self):
-        information_list = self.get_related_subject_list()
-        if self.redirected:
-            print('Redirected to subject: ' + str(self.dbpedia_url))
-        else:
-            print('Subject: ' + str(self.dbpedia_url))
+    def get_dbpedia_url(self):
+        return self.dbpedia_url
 
+    def print_all_information_of_subject(self):
+        information_list = self.get_related_subject_list()
+        print(self.dbpedia_url)
         for key in information_list:
             try:
                 print(key + ': ' + str(self.resource['http://dbpedia.org/ontology/' + key]))
             except KeyError:
-                None
-            except:
-                None
+                pass
---
+++ pyspotlight.py
@@ -0,0 +1,66 @@
+import spotlight
+import DBPediaRequest
+from pprint import pprint
+from nltk.tokenize import word_tokenize
+from nltk.util import ngrams
+
+
+def get_relevant_concepts_from_dbpedia(query: str) -> list:
+    return spotlight.candidates("http://model.dbpedia-spotlight.org/en/candidates", capitalize_all_words(query),
+                                confidence=0.3, support=20, spotter='Default')
+
+
+def get_n_resources_with_highest_support(n: int, candidates: list) -> list:
+    result_list = []
+    contained_uri_list = []
+    for candidate in candidates:
+        if len(result_list) < n:
+            candidate_uri = candidate['resource']['uri']
+            if not contained_uri_list.__contains__(candidate_uri):
+                result_list.append(candidate)
+                contained_uri_list.append(candidate_uri)
+        else:
+            for entry in result_list:
+                current_candidate_support = candidate['resource']['support']
+                current_entry_support = entry['resource']['support']
+                candidate_uri = candidate['resource']['uri']
+                entry_uri = entry['resource']['uri']
+                if current_candidate_support > current_entry_support and not contained_uri_list.__contains__(candidate_uri):
+                    result_list.remove(entry)
+                    contained_uri_list.remove(entry_uri)
+                    result_list.append(candidate)
+                    contained_uri_list.append(candidate_uri)
+
+    return result_list
+
+
+def print_information_of_dbpedia_concepts(dbpedia_concept_list: list):
+    for p in dbpedia_concept_list:
+        p.print_all_information_of_subject()
+
+
+def get_uris(candidates: list) -> list:
+    result_list = []
+    for candidate in candidates:
+        result_list.append(candidate['resource']['uri'])
+
+    return result_list
+
+
+def get_ngrams(text):
+    ngram_list = []
+    for i in range(1, len(text.split())+1):
+        n_grams = ngrams(word_tokenize(text), i)
+        ngram_list.append([ ' '.join(grams) for grams in n_grams])
+    return ngram_list
+
+
+def capitalize_all_words(s):
+    result = ""
+    s_list = s.split(' ')
+    for word in s_list:
+        result = result + word.capitalize() + ' '
+    return result.strip()
+
+
+pprint(get_relevant_concepts_from_dbpedia('president obama white house'))
