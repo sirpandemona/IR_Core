655e104d74bd4adb05b03d1e26b1805b79e39cc9
Vasco <vascodebruijn@gmail.com>
2019-03-11 17:31:33 +0100

    Cleaned up SVM class

--- SVM.py
+++ SVM.py
@@ -1,75 +1,48 @@
-from pprint import pprint
-
 from sklearn import svm, preprocessing
 import numpy as np
 from pyspotlight import get_relevant_concepts_from_dbpedia
 
 
-# dummy data
-# train_x = [[1], [2], [3], [4]]
-# train_y = [ 0, 0, 1,2]
-# test_x = [[0], [2.3], [5]]
+class SVM:
+    def __init__(self):
+        self.SVM = svm.SVC(gamma='scale', decision_function_shape='ovo', kernel='poly')
+        self.scaler = preprocessing.StandardScaler()
 
-# training the svm
-# scaler = preprocessing.StandardScaler().fit(train_x)
-# train_x_norm = scaler.transform(train_x)
-# clf = svm.SVC(gamma='scale', decision_function_shape='ovo',kernel='poly')
-# clf.fit(train_x_norm ,train_y)
+    def train(self, data, labels):
+        self.scaler = preprocessing.StandardScaler().fit(train_data)
+        norm_data = self.scaler.transform(data)
+        self.SVM.fit(norm_data, labels)
 
-# using the svm for classifying unknown objects
-# test_x_norm = scaler.transform(test_x)
-# unkw_y = clf.predict(test_x_norm)
-# print(unkw_y)
+    def evaluate(self, data) -> np.ndarray:
+        norm_data = self.scaler.transform(data)
+        labels = self.SVM.predict(norm_data)
+        return labels
 
-def concepts2feat(concept_list: list, query: str) -> np.ndarray:
-    rank = 0
-    n_rel_art = concept_list.__len__()
-    n_feats = 5
-    results = []
-    for candidates in concept_list:
-        res = candidates['resource']
-        if isinstance(res, dict):
-            res = [res]
-        n_res = res.__len__()
-        feature_vector = np.zeros((n_res, n_feats))
-        count = 0
-        for resource in res:
-            feature_vector[count, 0] = resource['contextualScore']
-            feature_vector[count, 1] = resource['finalScore']
-            feature_vector[count, 2] = resource['support']
-            feature_vector[count, 3] = rank
-            teq = resource['label'] == query  # title equals query
-            if teq:
-                feature_vector[count, 4] = 1
-            count = count+1
-        rank = rank + 1
-        if results.__len__() == 0:
-            results = feature_vector
-        else:
-            results = np.append(results, feature_vector, axis=0)
-    return results
-
-
-def generateTrainingSet(queries: list) -> np.ndarray:
-    training_data = []
-    for query in queries:
-        cl = get_relevant_concepts_from_dbpedia(query)
-        if cl:
-            feats = concepts2feat(cl, query)
-            if training_data.__len__() == 0:
-                training_data = feats
+    @staticmethod
+    def concepts2feat(concept_list: list, query: str) -> np.ndarray:
+        rank = 0
+        n_rel_art = concept_list.__len__()
+        n_feats = 5
+        results = []
+        for candidates in concept_list:
+            res = candidates['resource']
+            if isinstance(res, dict):
+                res = [res]
+            n_res = res.__len__()
+            feature_vector = np.zeros((n_res, n_feats))
+            count = 0
+            for resource in res:
+                feature_vector[count, 0] = resource['contextualScore']
+                feature_vector[count, 1] = resource['finalScore']
+                feature_vector[count, 2] = resource['support']
+                feature_vector[count, 3] = rank
+                teq = resource['label'] == query  # title equals query
+                if teq:
+                    feature_vector[count, 4] = 1
+                count = count + 1
+            rank = rank + 1
+            if results.__len__() == 0:
+                results = feature_vector
             else:
-                training_data = np.append(training_data, feats, axis=0)
-    training_data = np.array(training_data)
-    return training_data
-
-
-# query = "obama white house audience"
-# cl = get_relevant_concepts_from_dbpedia(query)
-qs = ["barrack obama speech white house", "donald trump speech white house"]
-train_data = generateTrainingSet(qs)
-training_labels = [1, 1, 0, 0]
-scaler = preprocessing.StandardScaler().fit(train_data)
-train_x_norm = scaler.transform(train_data)
-clf = svm.SVC(gamma='scale', decision_function_shape='ovo', kernel='poly')
-clf.fit(train_x_norm, training_labels)
+                results = np.append(results, feature_vector, axis=0)
+        return results
--- test_setup.py
+++ test_setup.py
@@ -1,18 +1,41 @@
 from pprint import pprint
 import SVM
+import pyspotlight
 
 
 def retrieve_queries() -> list:
     results = []
     file = open("topics.701-850.txt")
+    num = 701
     for line in file:
         if line.startswith("<title>"):
-            results.append(line.replace("<title>", "").replace("\n", ""))
+            q = line.replace("<title>", "").replace("\n", "")
+            results.append((q, num))
+            num = num + 1
     return results
 
 
-queries = retrieve_queries()
-dataset = SVM.generateTrainingSet(queries)
+def retrieve_concepts(queries: list) -> list:
+    results = []
+    for q in queries:
+        (query, num) = q
+        concept_list = pyspotlight.get_relevant_concepts_from_dbpedia(query)
+        results.append((query, num, concept_list))
+    return results
 
-pprint(dataset)
 
+def retrieve_features(concept_lists: list) -> list:
+    results = []
+    for cl in concept_lists:
+        (query, num, candidate) = cl
+        features = SVM.SVM.concepts2feat(candidate, query)
+        results.append((num, features))
+    return results
+
+
+qs = retrieve_queries()
+# pprint(qs)
+cls = retrieve_concepts(qs)
+pprint(cls)
+fts = retrieve_features(cls)
+pprint(fts)
